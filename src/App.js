19e6853c567e6d1272cd317423cd35c6812bac98
b2the <ohsung2000@gmail.com>
2022-04-01 19:30:05 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Customer from './components/Customer'
 import './App.css';
 
+const customers =[
+  {
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/1',
+  'name': '홍길동',
+  'birthday':'961222',
+  'gender':'남자',
+  'job': '대학생'
+},
+{
+  'id': 2,
+  'image': 'https://placeimg.com/64/64/2',
+  'name': '강감찬',
+  'birthday':'850403',
+  'gender':'남자',
+  'job': '장군'
+},
+{
+  'id': 3,
+  'image': 'https://placeimg.com/64/64/3',
+  'name': '김예지',
+  'birthday':'950312',
+  'gender':'여자',
+  'job': '디자이너'
+}
+]
 class App extends Component {
   render() {
     return (
-      <div className="gray-background">
-        <img src={logo} lat="logo" />
-        <h2>Let's develop managemnet system!</h2>
+      <div>
+        {customers.map(c => { return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} ob={c.job}/> ); })}
       </div>
     );
   }
